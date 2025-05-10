81ab1ee9e8017b33aab69e1d48c48382c24db595
Manobalaarul <manobalaarul3@gmail.com>
2025-05-10 19:53:08 +0530

    sales_server

---
+++ models/profile.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const profileSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "users",
+    },
+    company_name: {
+      type: String,
+      default: "",
+    },
+    address: {
+      type: String,
+      default: "",
+    },
+    phone: {
+      type: Number,
+      default: 1,
+    },
+    gstNo: {
+      type: String,
+      default: "",
+    },
+    gstPer: {
+      type: Number,
+      default: 1,
+    },
+    logo: {
+      type: String, // URL or file path
+      default: "",
+    },
+    date: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const ProfileModel = mongoose.model("profile", profileSchema);
+export default ProfileModel;
